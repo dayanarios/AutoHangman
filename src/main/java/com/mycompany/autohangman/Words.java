777f029e74840bd73dfa0c80ebedcf3217814c92
Dayana Rios <dayanarios05@gmail.com>
2019-10-06 15:14:44 -0700

    wordsapi get request

--- src/main/java/com/mycompany/autohangman/Words.java
+++ src/main/java/com/mycompany/autohangman/Words.java
@@ -5,10 +5,47 @@
  */
 package com.mycompany.autohangman;
 
+
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
+
 /**
  *
  * @author dayanarios
  */
 public class Words {
+    
+    private static String secretWord = " "; 
+    
+    
+
+    public static void getRequest(){
+        
+        try {
+            HttpResponse<JsonNode> response = Unirest.get("https://wordsapiv1.p.rapidapi.com/words/?random=true")
+                    .header("x-rapidapi-host", "wordsapiv1.p.rapidapi.com")
+                    .header("x-rapidapi-key", "5f02dbb470mshafbf646db901715p1c9a72jsn03d7d25d6d94")
+                    .asJson();
+            
+            extractSecretWord(response); 
+        } catch (UnirestException ex) {
+            Logger.getLogger(Words.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public static void extractSecretWord(HttpResponse<JsonNode> response){
+        JSONObject obj = response.getBody().getObject();
+        secretWord = obj.getString("word");
+        System.out.println(secretWord); 
+    }
+    
+    
+    
     
 }
